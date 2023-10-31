ec29af6c4c4445eb6486fcbc12f852df5cf1a4f2
FozilAzimov <foziljon.azimov@yandex.ru>
2023-10-31 17:02:20 +0500

    interview question

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-fetch("data.json").then(res => res.json()).then(res => {
+let btn = document.querySelector('.btn');
 
-  let table = document.querySelector('.table');
+btn.addEventListener('click', function () {
 
-  for (let i = 0; i < res.length; i++) {
-    let row = table.insertRow(i + 1);
-    let id = row.insertCell(0);
-    let name = row.insertCell(1);
-    let surname = row.insertCell(2);
-    let age = row.insertCell(3);
+  let inp = document.querySelector('.inp');
+  let file = inp.files[0];
+  let reader = new FileReader();
 
-    id.innerHTML = res[i].id;
-    name.innerHTML = res[i].name;
-    surname.innerHTML = res[i].surname;
-    age.innerHTML = res[i].age;
+  reader.onload = function ({ target: { result } }) {
+
+    let data = JSON.parse(result);
+
+    let table = document.querySelector('.table');
+
+    for (let i = 0; i < data.length; i++) {
+      let row = table.insertRow(i + 1);
+      let id = row.insertCell(0);
+      let name = row.insertCell(1);
+      let surname = row.insertCell(2);
+      let age = row.insertCell(3);
+
+      id.innerHTML = data[i].id;
+      name.innerHTML = data[i].name;
+      surname.innerHTML = data[i].surname;
+      age.innerHTML = data[i].age;
+    }
   }
-
+  reader.readAsText(file);
 });
+
+
+
 
 
